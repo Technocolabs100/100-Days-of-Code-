dc431613fa9f6aeafc66dc808d40c2c9fc9bfd3d
Technocolabs <50095209+Technocolabs@users.noreply.github.com>
2020-03-02 21:10:02 +0530

    Introduction to C++ and some basic Programs

---
+++ C++/Input.cpp
@@ -0,0 +1,20 @@
+
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "Enter your name: " << flush;
+	string input;
+	cin >> input;
+	cout << "You entered: " << input << endl;
+
+	cout << "Enter a number: " << flush;
+	int value;
+	cin >> value;
+	cout << "You entered: " << value << endl;
+
+	return 0;
+}
---
+++ C++/Variables.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int numberCats = 5;
+	int numberDogs = 7;
+	int numberAnimals = numberCats + numberDogs;
+
+	cout << "Number of cats: " << numberCats << endl;
+	cout << "Number of dogs: " << numberDogs << endl;
+
+	cout << "Total number of animals: " << numberAnimals << endl;
+
+	cout << "New dog acquired!" << endl;
+
+	numberDogs = numberDogs + 1;
+
+	cout << "New number of dogs: " << numberDogs << endl;
+
+	return 0;
+}
